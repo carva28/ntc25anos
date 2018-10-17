592b7914a4112938c86b05b3d2934ebbea33743d
mrsilvino <silvinomartins991@gmail.com>
2018-10-17 17:06:56 +0100

    added zoom-in and removed pan

--- js/3d_modelos.js
+++ js/3d_modelos.js
@@ -7,19 +7,25 @@ for (var i = 1993; i <= 2007; i++) {
     init(i);
 }
 
-
-
 function init(year) {
+
+    /* create variables */
     var mesh;
     var container, controls;
     var camera, scene, renderer, light;
 
+    /* create scene */
+    scene = new THREE.Scene();
+
+    /* get container reference */
     container = document.getElementById("ano" + year);
-    camera = new THREE.PerspectiveCamera(0.5, window.innerWidth / window.innerHeight, 0.25, 1000);
-    //camera.position.set( -1.8, 0.9, 2.7 );
-    camera.position.x = 0;
-    camera.position.y = 150;
-    camera.position.z = 10;
+
+    /* set camera settings */
+    camera = new THREE.PerspectiveCamera(0.25, window.innerWidth / window.innerHeight, 0.25, 250);
+    camera.position.set(0, 150, 50);
+
+    /* set controls */
+  
     controls = new THREE.OrbitControls(camera);
     controls.target.set(0, 0, 0);
     controls.enableRotate = true;
@@ -27,38 +33,29 @@ function init(year) {
     controls.maxPolarAngle = Math.PI * 0.5;
     controls.enableZoom = false;
     controls.enableKeys = false;
-
-    //controls.autoRotate = true;
+    controls.noPan = true;
     controls.update();
 
-
-
-    scene = new THREE.Scene();
-
+    /* set illumination */
     light = new THREE.HemisphereLight(0xbbbbff, 0x444422);
     light.position.set(1, 1, 0);
     scene.add(light);
 
 
 
-    //MODEL INITIAL
-
+    /* load model */
     var loader = new THREE.GLTFLoader();
     loader.load('models/' + year + '.glb', function (gltf) {
         console.log("carregou modelo");
         gltf.scene.traverse(function (child) {
-            if (child.isMesh) {
-
-
-            }
+            if (child.isMesh) { }
         });
 
-        gltf.scene.rotation.set(Math.PI * 0.5, 0, 0)
+        // rotate scene to object face forward
+        gltf.scene.rotation.set(Math.PI * 0.5, 0, 0);
 
         mesh = gltf.scene;
         scene.add(mesh);
-
-        mesh.rotation.y += 0.006;
 
         render();
 
@@ -77,19 +74,14 @@ function init(year) {
     animate();
 
     function animate() {
-
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
     }
 
-
     function render() {
-
-
         if (mesh) {
-            mesh.rotation.z -= 0.01;
+            mesh.rotation.z -= 0.006;
         }
-
         requestAnimationFrame(render);
     }
 
